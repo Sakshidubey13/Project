19c673d076788837d84ebacfdc165757b5383ae5
Dubey Sakshi <sakshidubey1304@gmail.com>
2026-05-23 00:18:28 +0530

    Update Login.jsx

--- auth-system/src/pages/Login.jsx
+++ auth-system/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import API from "../services/api";
 import { useDispatch } from "react-redux";
 import { login } from "../slices/authSlice";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 const Login = () => {
   const dispatch = useDispatch();
