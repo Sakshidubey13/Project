4ae0a24e98fa73bbf568d862c59137a70b208bd5
Dubey Sakshi <sakshidubey1304@gmail.com>
2026-05-22 19:46:48 +0530

    Update ProductRoutes.jsx

--- auth-system/src/routes/ProductRoutes.jsx
+++ auth-system/src/routes/ProductRoutes.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Navigate } from "react-router-dom";
 
-export default function ProductRoutes() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+export  default ProtectedRoute ({ children }) {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
