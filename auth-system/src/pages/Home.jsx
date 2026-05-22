efa6b5673714052d6fbf83be013267949f1bec49
Dubey Sakshi <sakshidubey1304@gmail.com>
2026-05-23 00:17:45 +0530

    Update Home.jsx

--- auth-system/src/pages/Home.jsx
+++ auth-system/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../slices/authSlice";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 const Home = () => {
   const user = useSelector((state) => state.auth.user);
