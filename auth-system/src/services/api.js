7deedbe373dd7953ee3799e8680d7a51a1d00dca
Dubey Sakshi <sakshidubey1304@gmail.com>
2026-05-22 23:51:53 +0530

    Update api.js

--- auth-system/src/services/api.js
+++ auth-system/src/services/api.js
@@ -1,9 +1,7 @@
-import React from "react";
 import axios from "axios";
 
-export default function api() {
-    const API = axios.create({
-        baseURL : "http://localhost:3000"
-    })
-  return <div></div>;
-}
+const API = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+export default API;
